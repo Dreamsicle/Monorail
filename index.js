a6c65185c60729b94e102d2698f9f091d8064b8a
danPixl <danny@vinci.cloud>
2017-02-13 08:14:54 -0700

    add link to gist

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ if (process.getuid && process.getuid() === 1000) {
 
 console.log("Manager started.")
 
-var servertype = require('./servertypes/' + config.servertype)
-servertype
+
+// I'd like to make a note that a lot of the work on the basic servers we already done by this guy:
+// https://gist.github.com/ryanflorence/701407
+// Mad props to him. 
+var server = require('./servertypes/' + config.servertype)
+server
 
 console.log("labHTTP running on port " + port + ".");

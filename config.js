f6bfdc4de00b010ea2a18c6c8e73e7e0b527eb95
danPixl <danny@vinci.cloud>
2017-03-03 23:23:18 -0700

    scrollbars

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {}
 // Change settings here
 
 // Website name and URL
-// By default, you can go to https://local.racklab.xyz (points to localhost) using the keys obtainable from the official page or using self-signed certs. 
+// By default, you can go to https://local.racklab.xyz (points to localhost) using the keys obtainable from the official page or using self-signed certs.
 
 const URL = 'https://local.racklab.xyz'
 const name = 'labHTTP Pivot'
@@ -17,12 +17,12 @@ const web_port = 443
 
 // SSL
 // Enter the full path.
-const key = ''
-const cert = ''
+const key = '/home/danPixl/.mySSL/Let\'s Encrypt/local.racklab/server.key'
+const cert = '/home/danPixl/.mySSL/Let\'s Encrypt/local.racklab/server.crt'
 
 // Password protection
-const username = "username"
-const password = "password"
+const username = 'username'
+const password = 'password'
 
 // Don't change anything beyond this point
 

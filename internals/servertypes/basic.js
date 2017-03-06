dd6dcbe1826cff13a5a3b7a56526be79250e94fa
danPixl <danny@vinci.cloud>
2017-03-05 21:56:24 -0700

    web app manifest; universal %variable% replacement

--- internals/servertypes/basic.js
+++ internals/servertypes/basic.js
@@ -54,10 +54,19 @@ http2.createServer(keys, function (request, response) {
         var processedpage = file.replace('%content%', convertmd.render(markdown)),
             processedpage = processedpage.replace('%title%', path.basename(filename).replace(/\.[^/.]+$/, ' - ') + config.websiteName),
             processedpage = processedpage.replace('%footer-backto%', '<a href="' + config.URL + '"><p class="backto">Back to  ' + config.websiteName + '</p></a>')
+        response.push(process.cwd() + '/content/css/page.css')
+        response.push(process.cwd() + '/content/css/prism.css')
+        response.push(process.cwd() + '/content/js/prism.js')
         response.write(processedpage, 'binary')
         response.end()
         })
       } else {
+        if (path.extname(filename).substring(0, 3) == ".js" ){ 
+          var file = file.replace(/%name%/ig, config.websiteName).replace(/%URL%/ig, config.URL)
+          response.write(file, 'binary')
+          response.end()
+          return
+        }
         response.write(file, 'binary')
         response.end()
       }

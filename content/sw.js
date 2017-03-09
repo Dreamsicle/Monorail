0cd020dc6ff530578580023e553d9122e9a2f4e1
danPixl <danny@vinci.cloud>
2017-03-09 07:43:05 -0700

    gt rid of old serviceworker code, clean the new stuff

--- content/sw.js
+++ content/sw.js
@@ -1,28 +1,16 @@
 // Stolen from https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
 
 var CACHE = '%name%-cache';
-// On install, cache some resources.
 self.addEventListener('install', function(evt) {
-
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
   evt.waitUntil(precache());
 });
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
 self.addEventListener('fetch', function(evt) {
   console.log('The service worker is serving the asset.');
-  // You can use `respondWith()` to answer immediately, without waiting for the
-  // network response to reach the service worker...
   evt.respondWith(fromCache(evt.request));
-  // ...and `waitUntil()` to prevent the worker from being killed until the
-  // cache is updated.
   evt.waitUntil(update(evt.request));
 });
 
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
@@ -34,9 +22,6 @@ function precache() {
   });
 }
 
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
@@ -45,8 +30,6 @@ function fromCache(request) {
   });
 }
 
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
 function update(request) {
   return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
@@ -54,65 +37,3 @@ function update(request) {
     });
   });
 }
-
-// Old cache
-/*
-var CACHE_NAME = '%name%-cache';
-var urlsToCache = [
-  '/',
-  '/css/page.css',
-  '/css/prism.css',
-  '/js/prism.js'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-*/
